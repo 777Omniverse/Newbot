f9cdb7116e9d4077a660e8444e5e6255d5a411bc
Sam <117886422+777Omniverse@users.noreply.github.com>
2024-03-28 11:51:15 +0530

    Update and rename bot (1).js to bot.js

--- modules/commands/bot.js
+++ modules/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🚧${name}🚧,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🚧${name}🚧,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 :   𝐉⃝   ̶  ̶  ̶𝐚͡𝐜͜𝐤𝐬𝐨𝐧ﮩﮩ٨ــﮩــــ𓆩♥︎𓆪🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
