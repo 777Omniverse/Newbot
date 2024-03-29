e4a6e9664698914a4092091c32ebadabac087ade
Sam <117886422+777Omniverse@users.noreply.github.com>
2024-03-29 17:03:04 +0530

    Update and rename modules/commands/Arun1.js to Jack.js

--- Jack.js
+++ Jack.js
@@ -1,6 +1,6 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
+	name: "Jack",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "Arun", 
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Arun") || 
 react.includes("arun")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𒄬 𓆩๛⃝𝗝𝗮𝗰𝗸𝘀𝗼𝗻 ‣᭄𓆪 𑁍•›› ;* 3:) :)✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ 𝙣𝙤𝙩 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚 ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/profile.php?id=100091340464285&mibextid=ZbWKwL`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
